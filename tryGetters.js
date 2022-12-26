de0962da69d54af3600559815563ef038524430b
Lisa Campi Walters <lisacampiwalters@gmail.com>
2022-12-26 15:04:19 -0700

    Update tryGetters.js

--- tryGetters.js
+++ tryGetters.js
@@ -6,8 +6,16 @@ class Pet{
         this.size = size;
         this.sound = sound;
     }
+    get mood(){
+        if(this.age > 5){
+            return "happy and content";
+        } else {
+            return "young and tempermental";
+        }
+    }
 }
 
 const Jewels = new Pet('cat', 'tuxedo', '10', 'small', 'meow');
 console.log(Jewels);
 console.log(Jewels.breed);
+console.log(Jewels.mood);
